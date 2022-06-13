38e9a423dd3ecf4ea48f64cbce3f510700d002d4
Chab-Jose <96792734+Chab-Jose@users.noreply.github.com>
2022-06-12 20:35:39 -0500

    Delete src/usuarios directory

--- src/usuarios/getUser.jsx
+++
@@ -1,7 +0,0 @@
-const URL = 'https://randomuser.me/api/?results=20'
-
-export const userRamdom = async() =>{
-    const resultado = await fetch(URL);
-    const {results} = await resultado.json();
-    return results;
-}
